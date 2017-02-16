5fe1058ee8cc1868826b9fb947b22654889721bb
Nathan Mellis <Mellis_Nathan@bah.com>
2017-02-16 17:37:45 -0500

    Added a Sidebar component.

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+import Link from "./Link";
+
+export class Sidebar extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+  }
+  
+  render()
+  {
+    return (
+      <ul className="usa-sidenav-list">
+        {this.props.children}
+      </ul>
+    )
+  }
+}
+
+
+// =============================================================================
+
+
+export class SidebarLink extends React.Component
+{
+  renderChildren()
+  {
+    let children = "";
+    if (React.Children.count(this.props.children) > 0) {
+      children = (
+        <ul className="usa-sidenav-sub_list">
+          {this.props.children}
+        </ul>
+      )
+    }
+    return children;
+  }
+  
+  render()
+  {
+    return (
+      <li>
+        <Link {...this.props}>{this.props.label}</Link>
+        {this.renderChildren()}
+      </li>
+    )
+  }
+}
+
+SidebarLink.propTypes = {
+  label: React.PropTypes.string.isRequired
+};
