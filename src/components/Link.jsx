aebfb4f37c3ade358c1416697561b355fefeceb5
Nathan Mellis <Mellis_Nathan@bah.com>
2017-02-16 17:36:14 -0500

    Added a Link class.
    This wraps the react-router Link class by automatically adding the `activeClassName` prop if it hasn’t already been specified.

---
+++ src/components/Link.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link as RRLink } from "react-router";
+
+export default class Link extends React.Component
+{
+  render()
+  {
+    let props = Object.assign({}, this.props);
+    if (!props.activeClassName) {
+      props.activeClassName = "usa-current";
+    }
+    return (
+      <RRLink {...props} />
+    )
+  }
+}
