71b606eb5ade6a26d0d018ed8f76f0860bc00176
Michael Stanaland <mstanaland@mac.com>
2017-02-27 07:01:22 -0500

    Fixes JSDoc syntax

--- lib/components/InputLabel.jsx
+++ lib/components/InputLabel.jsx
@@ -2,11 +2,11 @@ import React, { PropTypes } from 'react';
 
 /**
  * Label for form fields
- * @param {string, required} label text for the input's label
- * @param {string, required} htmlFor  sets the <label for... attribute
+ * @returns {node} the rendered DOM node
+ * @param {string} label required - text for the input's label
+ * @param {string} htmlFor required - sets the <label for... attribute
  * @param {bool} required defaults to false. Adds required label
  */
-
 export default function InputLabel ({label, htmlFor, required}) {
   return (
     <label
