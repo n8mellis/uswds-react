859c4a41190af0dff4314945f81ca950027d7a10
Nathan Mellis <Mellis_Nathan@bah.com>
2017-03-02 14:34:07 -0500

    Fixed linting issues.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-var gulp = require('gulp'),
+let gulp = require('gulp'),
   $ = require('gulp-load-plugins')({ camelize: true }),
   runSequence = require('run-sequence'),
   filter = require("gulp-filter"),
-  rename = require("gulp-rename")
+  rename = require("gulp-rename");
 
-var normalizeCssFilter = $.filter('normalize.css', { restore: true });
+let normalizeCssFilter = $.filter('normalize.css', { restore: true });
 
 gulp.task('css', function() {
   return gulp.src('styles/main.scss')
@@ -16,37 +16,37 @@ gulp.task('css', function() {
            }))
            .pipe($.sourcemaps.write())
            .pipe(gulp.dest('./dist'))
-})
+});
 
 gulp.task('css:watch', ['css'], function() {
   gulp.watch('styles/**/*.scss', ['css'])
-})
+});
 
 gulp.task('moveAssets', function() {
   return gulp.src('./assets/**/*')
-             .pipe(gulp.dest('./dist/assets'))
-})
+             .pipe(gulp.dest('./dist/assets'));
+});
 
 gulp.task('build:revAssets', ['css', 'moveAssets'], function() {
-  var rev = new $.revAll()
+  let rev = new $.revAll();
   return gulp.src('./dist/**/*')
              .pipe(rev.revision())
              .pipe(gulp.dest('./dist/public'))
              .pipe(rev.manifestFile())
              .pipe(gulp.dest('./dist'))
-})
+});
 
 gulp.task('build:cpServer', function() {
   return gulp.src('./{styleguide,lib,server}/**/*.{js,jsx,ejs}')
              .pipe(gulp.dest('./dist/server-build'))
-})
+});
 gulp.task('build:revServer', ['build:cpServer'], function() {
-  var manifest = gulp.src('./dist/rev-manifest.json')
+  let manifest = gulp.src('./dist/rev-manifest.json');
   return gulp.src('./dist/server-build/{components,containers}/**/*')
              .pipe($.revReplace({ manifest: manifest }))
              .pipe(gulp.dest('./dist/server-build'))
-})
+});
 
 gulp.task('build', function() {
   runSequence('build:revAssets', 'build:revServer')
-})
+});
