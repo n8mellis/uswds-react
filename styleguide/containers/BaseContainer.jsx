e72cef2bb202565bc1baa8a1863b0d0f7c7424f5
Nathan Mellis <Mellis_Nathan@bah.com>
2017-03-06 17:30:55 -0500

    Updated BaseContainer to render each section.
    To ensure consistency across all the different sections, the BaseContainer is now designed to be subclassed rather than composed in.  Each of the different sections, Example, Code, and Documentation all have their own render method that is called by the main render method to ensure that everything comes together correctly.  This should remove nearly all the boilerplate code necessary in each component container page.
    
    NOTE: This will require all the current containers to be updated.
    
    In the future, I would like to figure out a way that we can get the sample code and example using the same information so that it automatically updates both when it is changed.  That requires some wizardry I haven’t figured out yet, however, so for the moment, copy and paste it is.

--- styleguide/containers/BaseContainer.jsx
+++ styleguide/containers/BaseContainer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Accordion, AccordionItem } from "../../lib/components/Accordion";
+
 /**
  * Provides a common container for all Styleguide sub pages.
  *
@@ -24,6 +26,49 @@ import React from "react";
 export default class BaseContainer extends React.Component
 {
   /**
+   * Renders the preview.
+   */
+  renderPreview()
+  {
+    return (<p>Preview goes here.</p>);
+  }
+
+  /**
+   * Renders the sample code segment.
+   *
+   * @returns {Node} The render tree for the sample code section.
+   */
+  renderSampleCode()
+  {
+    let code = "Sample code goes here.";
+
+    return (
+      <div className="highlight">
+        <pre>
+          <code className="language-html" data-lang="html">
+            {code}
+          </code>
+        </pre>
+      </div>
+    );
+  }
+
+  /**
+   * Renders the documentation.
+   *
+   * By default, this method will return the children of this component since
+   * the other rendered elements are handled with props.
+   *
+   * To change this behavior, subclass this component and override this method.
+   *
+   * @returns {Node} The render tree for the documentation section.
+   */
+  renderDocumentation()
+  {
+    return this.props.children;
+  }
+
+  /**
    * Renders the component.
    *
    * @returns {Node|null} The rendered DOM node.
@@ -39,9 +84,22 @@ export default class BaseContainer extends React.Component
         <p className="usa-font-lead">
           {this.props.summary}
         </p>
-        
-        {this.props.children}
 
+        <div className="preview">
+          {this.renderPreview()}
+        </div>
+
+        <Accordion type={Accordion.TYPE_BORDERED} startCollapsed>
+          <AccordionItem title="Code">
+            {this.renderSampleCode()}
+          </AccordionItem>
+        </Accordion>
+
+        <Accordion type={Accordion.TYPE_BORDERED}>
+          <AccordionItem title="Documentation" expanded>
+            {this.renderDocumentation()}
+          </AccordionItem>
+        </Accordion>
       </div>
     );
   }
